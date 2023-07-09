9b7b4b3729ac1892703f96e3438d996866ffa47e
Parijat-Majumder <115878618+Parijat-Majumder@users.noreply.github.com>
2023-07-09 17:13:15 +0530

    Create medium_lastques.sql

---
+++ medium_lastques.sql
@@ -0,0 +1,20 @@
+/*
+Display patient's full name,
+height in the units feet rounded to 1 decimal,
+weight in the unit pounds rounded to 0 decimals,
+birth_date,
+gender non abbreviated.
+
+Convert CM to feet by dividing by 30.48.
+Convert KG to pounds by multiplying by 2.205
+*/
+select
+concat(first_name , ' ', last_name)as fullname , 
+round((height/30.48),1) as height ,
+round( (weight * 2.205),0) as weight ,
+birth_date,
+CASE 
+  When gender = 'M' THEN 'MALE'
+  ELSE 'FEMALE'
+ END AS gender_type 
+from patients;
